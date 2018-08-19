cd66824ea503ad092c0d47e6686060a201145e43
Ava Barron <mztriz@gmail.com>
2018-08-19 02:13:43 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Alicia Keys: "Never Felt This Way",
+  Lauryn Hill : "Zion",
+  India Arie : "Little Things"
+}
+
+function updatePlaylist(playlist, artist, songTitle){
+  return playlist.artist = [songTitle]
+}
